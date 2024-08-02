d1a1f93ed2c74ad569ae2f34dab0ff0b2437d679
Sidharth Kumar <sidharthkumar@Sidharths-MacBook-Pro.local>
2024-08-02 18:45:49 +0530

    Second COmmit

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -12,4 +12,34 @@ router.get('/bfhl', (req, res) => {
     }
 });
   
+
+router.post('/bfhl', (req, res) => {
+    const { data } = req.body;
+    const user_id = "Sidharth_Kumar_01102002";
+    const email = "sm1195@srmist.edu.in";
+    const roll_number = "RA2111026010007";
+  
+    if (!data || !Array.isArray(data)) {
+      return res.status(400).json({ is_success: false });
+    }
+  
+    const numbers = data.filter(item => !isNaN(item));
+    const alphabets = data.filter(item => /^[a-zA-Z]$/.test(item));
+    const highest_alphabet = alphabets.length > 0 ? [alphabets.sort().pop()] : [];
+
+    if (highest_alphabet.length > 0) {
+        alphabets.push(highest_alphabet[0]);
+      }
+    
+  
+    res.json({
+      is_success: true,
+      user_id,
+      email,
+      roll_number,
+      numbers,
+      alphabets,
+      highest_alphabet
+    });
+  });
 module.exports = router;
